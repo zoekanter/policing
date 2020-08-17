ad644c45d07061051079ab84788a2dea4162d3bd
khsavage <katiesavage2018@gmail.com>
2020-08-17 13:27:50 -0700

    updating

--- Austin code.R
+++ Austin code.R
@@ -1,6 +1,7 @@
 #calling libraries
 library(dplyr)
 library(tidyr)
+library(gtools)
 
 #set working directory
 setwd("C:/Users/katie/Desktop/policing/Austin Dirty Datasets")
@@ -19,6 +20,49 @@ Citations18<-read.csv(file='2018_RP__Citations.csv', stringsAsFactors = FALSE)
 Shooting_Incidents<-read.csv(file='Officer_Involved_Shootings_2008-17_Incidents.csv', stringsAsFactors = FALSE)
 Shooting_officers<-read.csv(file='Officer_Involved_Shootings_2008-17_Officers.csv', stringsAsFactor = FALSE)
 Shooting_Subjects<-read.csv(file='2008-17_OIS_Subjects.csv', stringsAsFactors = FALSE)
+
+
+#making columns the same so they merge easily
+UOF14_Fix_Col<-subset(UOF14, select=-c(Subject.Effects))
+UOF15_Fix_COl<-subset(UOF15, select=-c(Subject.Effects))
+UOF16_Fix_Col<-subset(UOF16, select=-c(Subject.Effects))
+UOF17_Fix_Col<-UOF17[, -c(29:32)]
+UOF18_Fix_Col<-UOF18[, -c(29:32)]
+
+#changing column names to match
+UOF16_Fix_Col<-UOF16_Fix_Col %>% rename(Date.Occurred = Date..Occurred,
+                                        Time.Occurred = Time..Occurred,
+                                        Area.Command = Area..Command,
+                                        Subject.Sex = Subject..Sex,
+                                        Subject.Race = Subject..Race,
+                                        Subject.Ethnicity = Subject..Ethnicity,
+                                        Number.Shots = Number..Shots,
+                                        Officer.Organization.Desc = Officer..Organization.Desc,
+                                        Officer.Commission.Date = Officer..Commission.Date)
+UOF17_Fix_Col<-UOF17_Fix_Col %>% rename(Date.Occurred = Date..Occurred,
+                         Time.Occurred = Time..Occurred,
+                         Area.Command = Area..Command,
+                         Subject.Sex = Subject..Sex,
+                         Subject.Race = Subject..Race,
+                         Subject.Ethnicity = Subject..Ethnicity,
+                         Number.Shots = Number..Shots,
+                         Officer.Organization.Desc = Officer..Organization.Desc,
+                         Officer.Commission.Date = Officer..Commission.Date)
+
+
+UOF18_Fix_Col<-UOF18_Fix_Col%>% rename(Date.Occurred = Date..Occurred,
+                        Time.Occurred = Time..Occurred,
+                        Area.Command = Area..Command,
+                        Subject.Sex = Subject..Sex,
+                        Subject.Race = Subject..Race,
+                        Subject.Ethnicity = Subject..Ethnicity,
+                        Number.Shots = Number..Shots,
+                        Officer.Organization.Desc = Officer..Organization.Desc,
+                        Officer.Commission.Date = Officer..Commission.Date)
+
+
+#merge em all together??
+UOF_ALL<-smartbind(UOF14_Fix_Col,UOF15_Fix_COl,UOF16_Fix_Col,UOF17_Fix_Col,UOF18_Fix_Col)
 
 
 #numerize the data
@@ -55,7 +99,6 @@ SplitDateTime_UOF14<-as.data.frame(SplitDateTime_UOF14, stringsAsFactors=FALSE)
 #table with relevant data for 2014
 ALLMetadata_UOF14<-cbind.data.frame(UOF14$RIN,SplitDateTime_UOF14$date,UOF14$Time.Occurred,UOF14$Subject.Race,UOF14$Subject.Sex,UOF14$Officer.Yrs.of.Service,UOF14$Officer.Commission.Date,UOF14$Officer.Organization.Desc,UOF14$Area.Command, stringsAsFactors=FALSE)
 colnames(ALLMetadata_UOF14)<-(c("RIN","Date","Time","Subject Race","Subject Sex","Officer Yrs of Service","Officer Commision Date","Officer Organization Desc","Area Command"))
-
 
 
 #separating date and time in citations to get rid of unnecessary 00:00
