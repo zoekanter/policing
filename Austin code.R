3d31ac7606f8b4db3a1da6dce70ac67168ee86fb
khsavage <katiesavage2018@gmail.com>
2020-08-18 03:13:58 -0700

    first draft of cleaning script

--- Austin code.R
+++ Austin code.R
@@ -12,7 +12,7 @@ UOF14<-read.csv(file='R2R_2014.csv', stringsAsFactors = FALSE)
 UOF16<-read.csv(file='R2R_2016.csv', stringsAsFactors = FALSE)
 UOF17<-read.csv(file='R2R_2017.csv', stringsAsFactors = FALSE)
 UOF18<-read.csv(file='R2R_2018.csv', stringsAsFactors = FALSE)
-citations<-read.csv(file='Racial_Profiling_Dataset_2015-_Citations.csv', stringsAsFactors = FALSE)
+citations15<-read.csv(file='Racial_Profiling_Dataset_2015-_Citations.csv', stringsAsFactors = FALSE)
 citations14<-read.csv(file='2014_RP__Citations.csv', stringsAsFactors = FALSE)
 citations16<-read.csv(file='2016_RP__Citations.csv', stringsAsFactors = FALSE)
 citations17<-read.csv(file='2017_RP__Citations.csv', stringsAsFactors = FALSE)
@@ -61,54 +61,58 @@ UOF18_Fix_Col<-UOF18_Fix_Col%>% rename(Date.Occurred = Date..Occurred,
                         Officer.Commission.Date = Officer..Commission.Date)
 
 
-#merge em all together??
+#bind them all together
 UOF_ALL<-smartbind(UOF14_Fix_Col,UOF15_Fix_COl,UOF16_Fix_Col,UOF17_Fix_Col,UOF18_Fix_Col)
 
 
 #numerize the data
-#splitting year and date into  different variables UOF15
-SplitDateTime_UOF15<-strsplit(as.character(UOF15$Date.Occurred), "\\s")
-SplitDateTime_UOF15<-do.call(rbind, SplitDateTime_UOF15)
-colnames(SplitDateTime_UOF15)<-(c("date", "time"))
-SplitDateTime_UOF15<-as.data.frame(SplitDateTime_UOF15, stringsAsFactors=FALSE)
+#splitting year and date into  different variables
+SplitDateTime_UOF_ALL<-strsplit(as.character(UOF_ALL$Date.Occurred), "\\s")
+SplitDateTime_UOF_ALL<-do.call(rbind, SplitDateTime_UOF_ALL)
+colnames(SplitDateTime_UOF_ALL)<-(c("date", "time"))
+SplitDateTime_UOF_ALL<-as.data.frame(SplitDateTime_UOF_ALL, stringsAsFactors=FALSE)
 #making a tables with relevant metadata
-ALLMetadata_UOF15<-cbind.data.frame(UOF15$RIN,SplitDateTime_UOF15$date,UOF15$Time.Occurred,UOF15$Subject.Race,UOF15$Subject.Sex,UOF15$Officer.Yrs.of.Service,UOF15$Officer.Commission.Date,UOF15$Officer.Organization.Desc,UOF15$Area.Command, stringAsFactors=FALSE)
-colnames(ALLMetadata_UOF15)<-(c("RIN", "date","time","subject race","subject sex","officer Yrs of service","officer commision date","officer organization Desc","Area Command"))
+ALLMetadata_UOF<-cbind.data.frame(UOF_ALL$RIN,SplitDateTime_UOF_ALL$date,UOF_ALL$Time.Occurred,UOF_ALL$Subject.Race,UOF_ALL$Subject.Ethnicity,UOF_ALL$Subject.Sex,UOF_ALL$Officer.Yrs.of.Service,UOF_ALL$Officer.Commission.Date,UOF_ALL$Officer.Organization.Desc,UOF_ALL$Area.Command, stringAsFactors=FALSE)
+colnames(ALLMetadata_UOF)<-(c("RIN", "date","time","subject race","Subject Ethnicity","subject sex","officer Yrs of service","officer commision date","officer organization Desc","Area Command"))
 
 #make all null values = NA for UOF
-ALLMetadata_UOF15_NA<-ALLMetadata_UOF15
-ALLMetadata_UOF15_NA[ALLMetadata_UOF15=="u"]<-NA
-ALLMetadata_UOF15_NA[ALLMetadata_UOF15==""]<-NA
-ALLMetadata_UOF15_NA[ALLMetadata_UOF15=="-1"]<-NA
+ALLMetadata_UOF_NA<-ALLMetadata_UOF_ALL
+ALLMetadata_UOF_NA[ALLMetadata_UOF_ALL=="u"]<-NA
+ALLMetadata_UOF_NA[ALLMetadata_UOF_ALL==""]<-NA
+ALLMetadata_UOF_NA[ALLMetadata_UOF_ALL=="-1"]<-NA
 
 #changing race to be words instead of single letters & to match Citations
-AllMetadata_UOF15_FixRace<-ALLMetadata_UOF15_NA
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="W"]<-("White")
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="B"]<-("Black")
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="A"]<-("Asian")
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="H"]<-("Hispanic")
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="I"]<-("Native American")
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="M"]<-("Middle Eastern")
-AllMetadata_UOF15_FixRace[ALLMetadata_UOF15=="P"]<-("Hawaiian or Pacific Islander")
+AllMetadata_UOF_FixRace<-ALLMetadata_UOF_NA
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="W"]<-("White")
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="B"]<-("Black")
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="A"]<-("Asian")
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="H"]<-("Hispanic")
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="I"]<-("Native American")
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="M"]<-("Middle Eastern")
+AllMetadata_UOF_FixRace[ALLMetadata_UOF_ALL=="P"]<-("Hawaiian or Pacific Islander")
 
-#UOF14
-SplitDateTime_UOF14<-strsplit(as.character(UOF14$Date.Occurred), "\\s")
-SplitDateTime_UOF14<-do.call(rbind, SplitDateTime_UOF14)
-colnames(SplitDateTime_UOF14)<-(c("date", "time"))
-SplitDateTime_UOF14<-as.data.frame(SplitDateTime_UOF14, stringsAsFactors=FALSE)
-#table with relevant data for 2014
-ALLMetadata_UOF14<-cbind.data.frame(UOF14$RIN,SplitDateTime_UOF14$date,UOF14$Time.Occurred,UOF14$Subject.Race,UOF14$Subject.Sex,UOF14$Officer.Yrs.of.Service,UOF14$Officer.Commission.Date,UOF14$Officer.Organization.Desc,UOF14$Area.Command, stringsAsFactors=FALSE)
-colnames(ALLMetadata_UOF14)<-(c("RIN","Date","Time","Subject Race","Subject Sex","Officer Yrs of Service","Officer Commision Date","Officer Organization Desc","Area Command"))
+#fixing column names to match for easy combining
+Citations18<-Citations18%>% rename(OFF.FROM.DATE = OffenseDate,
+                                   CITATION.NUMBER = Citation.Number,
+                                     OFF.TIME = OffenseTime,
+                                     RACE.ORIGIN.CODE = Race,
+                                     CASE.PARTY.SEX = Sex,
+                                     RACE.KNOWN = Race_Known,)
+
+
+
+#combining all citations data
+Citations_ALL<-smartbind(citations14,citations15,citations16,citations17,Citations18)
 
 
 #separating date and time in citations to get rid of unnecessary 00:00
-SplitDateTime_Citations<-strsplit(as.character(citations$OFF.FROM.DATE), "\\s")
+SplitDateTime_Citations<-strsplit(as.character(Citations_ALL$OFF.FROM.DATE), "\\s")
 SplitDateTime_Citations<-do.call(rbind, SplitDateTime_Citations)
 colnames(SplitDateTime_Citations)<-(c("date","time"))
 SplitDateTime_Citations<-cbind.data.frame(SplitDateTime_Citations, stringsAsFactors=FALSE)
 
 #making a table with all relevant metadata for citations
-AllMetadata_Citations<-cbind.data.frame(SplitDateTime_Citations$date,citations$OFF.TIME,citations$RACE.ORIGIN.CODE,citations$CASE.PARTY.SEX, stringsAsFactors=FALSE)
+AllMetadata_Citations<-cbind.data.frame(SplitDateTime_Citations$date,Citations_ALL$OFF.TIME,Citations_ALL$RACE.ORIGIN.CODE,Citations_ALL$CASE.PARTY.SEX, stringsAsFactors=FALSE)
 colnames(AllMetadata_Citations)<-(c("date","time","Subject Race","Subject gender"))
 
 #make all empty values = NA
@@ -124,4 +128,12 @@ AllMetadata_citations_FixRace[AllMetadata_Citations=="A"]<-("Asian")
 AllMetadata_citations_FixRace[AllMetadata_Citations=="H"]<-("Hispanic")
 AllMetadata_citations_FixRace[AllMetadata_Citations=="ME"]<-("Middle Eastern")
 AllMetadata_citations_FixRace[AllMetadata_Citations=="N"]<-("Native American")
+
+#combining shootings into one database with all info together (multiple entries for single case numbers problem, also definitely a better way to do this)
+OIS_ALL<-merge(Shooting_officers,Shooting_Subjects)
+OIS_ALL<-merge(OIS_ALL,Shooting_Incidents)
+
+#make a table with availible data
+AllMetadata_Shootings<-cbind.data.frame(OIS_ALL$Case..,OIS_ALL$Date,OIS_ALL$Time,OIS_ALL$Officer.Name,OIS_ALL$Rank,OIS_ALL$Officer.Race.Ethnicity,OIS_ALL$Officer.Gender,OIS_ALL$Prev.OIS,OIS_ALL$Subject.Race.Ethnicity,OIS_ALL$Subject.Gender, stringsAsFactors = FALSE)
+colnames(AllMetadata_Shootings)<-(c("Case","Date","Time","Officer Name","Officer Rank","Officer Race","Officer Gender","Officer Prev. OIS","Subject Race","Subject Gender"))
 
